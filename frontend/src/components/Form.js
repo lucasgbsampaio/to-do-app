4e44debbe472eec5e44c2c22b0a2fe9a334e39e3
Lucas <lukas-gabriel2002@hotmail.com>
2020-11-21 18:29:54 -0300

    update

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 
 import { USER_CREATE, USER_LOGIN } from '../services/api';
-import Background from '../assets/signup-image.jpg';
 
 import style from './styles/Form.module.css';
 
@@ -59,76 +58,65 @@ export default function Form({ type }) {
   }, []);
 
   return (
-    <div
-      style={
-        type === 'Registrar'
-          ? {
-              background: 'none',
-              backgroundImage: `url(${Background})`,
-              backgroundRepeat: 'no-repeat',
-              backgroundColor: '#fff',
-              backgroundPosition: '85% center',
-            }
-          : null
-      }
-      className={style.wrapper}
-    >
-      <h1>{type}</h1>
-      <form onSubmit={handleSubmit} autoComplete="off">
-        <div>
-          <label className={style.label} htmlFor="username">
-            Usuário:
-          </label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            value={username}
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-
-        <div>
-          <label className={style.label} htmlFor="password">
-            Senha:
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-
-        <div>
-          {!loading ? (
-            <input type="submit" value={type} />
-          ) : (
+    <>
+      <div className={style.wrapper}>
+        <h1>{type}</h1>
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <div>
+            <label className={style.label} htmlFor="username">
+              Usuário:
+            </label>
             <input
-              style={{
-                fontSize: '16px',
-                opacity: '0.5',
-                cursor: 'not-allowed',
-              }}
-              readOnly
-              type="submit"
-              value="Carregando..."
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
             />
-          )}
-          {error && (
-            <span style={{ marginLeft: '15px', fontSize: '14px' }}>
-              {error}
-            </span>
-          )}
-        </div>
+          </div>
 
-        {type === 'Entrar' ? (
-          <NavLink to="/register">Não possui uma conta?</NavLink>
-        ) : (
-          <NavLink to="/login">Já possui uma conta?</NavLink>
-        )}
-      </form>
-    </div>
+          <div>
+            <label className={style.label} htmlFor="password">
+              Senha:
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+
+          <div>
+            {!loading ? (
+              <input type="submit" value={type} />
+            ) : (
+              <input
+                style={{
+                  fontSize: '16px',
+                  opacity: '0.5',
+                  cursor: 'not-allowed',
+                }}
+                readOnly
+                type="submit"
+                value="Carregando..."
+              />
+            )}
+            {error && (
+              <span style={{ marginLeft: '15px', fontSize: '14px' }}>
+                {error}
+              </span>
+            )}
+          </div>
+
+          {type === 'Entrar' ? (
+            <NavLink to="/register">Não possui uma conta?</NavLink>
+          ) : (
+            <NavLink to="/login">Já possui uma conta?</NavLink>
+          )}
+        </form>
+      </div>
+    </>
   );
 }
