02c15ca3873c8f46863c0f6675f841f199316611
Lucas <lukas-gabriel2002@hotmail.com>
2020-11-23 14:22:59 -0300

    update

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -5,9 +5,5 @@ import Form from '../components/Form';
 import style from './styles/Register.module.css';
 
 export default function Register() {
-  return (
-    <div className={style.contentMainRegister}>
-      <Form type="Registrar" />
-    </div>
-  );
+  return <div></div>;
 }
