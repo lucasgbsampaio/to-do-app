2557d3562e0cd04ff445b6b8d475f553f8ad34ba
Lucas <lukas-gabriel2002@hotmail.com>
2020-11-24 19:00:23 -0300

    update

--- src/server.js
+++ src/server.js
@@ -15,10 +15,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build/static')));
 app.use('/api', routes);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
 const { DB_USER, DB_PASSWORD, DB_NAME } = process.env;
